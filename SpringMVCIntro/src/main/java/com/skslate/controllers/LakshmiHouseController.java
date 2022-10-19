3af25eb8782aebb66bd6b7094093c2a912bae7cf
kishore reddy <kishorekumarreddykadapa@gmail.com>
2022-10-19 20:40:19 +0530

    spring view commit

--- SpringMVCIntro/src/main/java/com/skslate/controllers/LakshmiHouseController.java
+++ SpringMVCIntro/src/main/java/com/skslate/controllers/LakshmiHouseController.java
@@ -1,6 +1,11 @@
 package com.skslate.controllers;
 
+import java.util.Date;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -8,10 +13,18 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @RequestMapping("lakshmiHouse")
 public class LakshmiHouseController {
 
-	@ResponseBody
-	@RequestMapping("/sisterGift")
-	public String sister() {
-		return "Sister Received Makeup kit ... sent from US ...";
+	@RequestMapping("/gitStatus")
+	public String sister(Model model, HttpServletRequest  req) {
+		
+		String giftName = req.getParameter("giftName");
+		String toWhom = req.getParameter("toWhom");
+		String price = req.getParameter("price");
+		model.addAttribute("toWhom",toWhom);
+		model.addAttribute("giftPrice",price);
+		model.addAttribute("giftName",giftName);
+		model.addAttribute("giftSentDate",new Date());
+		
+		return "Response";
 	}
 	
 	@ResponseBody
